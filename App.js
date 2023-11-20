34db9f9449e0edda6deccaa352fb58e70f964892
ThoNguyen <thonguyen0110.work@gmail.com>
2023-11-20 23:02:42 +0700

    commit code

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Carousel from "react-native-reanimated-carousel/src/Carousel";
 export default function App() {
+  const images = []
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
